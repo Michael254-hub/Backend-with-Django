01adfb3afe8cf628260150671451a91fe0c4fd3b
Michael254-hub <michaelsimiyu54@gmail.com>
2024-11-15 15:08:22 +0300

    progress

--- BackApp/urls.py
+++ BackApp/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from BackApp import views
 
-urlpatterns[
+urlpatterns = [
 
     path('',views.blog_list,name='blog_list'),
 
-    path('<int;id>',views.blog_details,name='blog_details'),
+    path('<int:id>',views.blog_details,name='blog_details'),
 ]
